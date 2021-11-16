0679b2f09f7f24fe2f914cc60d8798bb27e94b28
Johnathan Mahones <jmthefox@live.com>
2021-11-16 17:36:55 -0500

    Uploaded items before clean install.

---
+++ Hosni Face/Assets/Scripts/ColorControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorControl : MonoBehaviour
+{
+    public GameObject 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKey(KeyCode.D))
+      {
+          transform.Translate(speed * Time.deltaTime, 0, 0);
+      }
+
+      if (Input.GetKey(KeyCode.A))
+      {
+          transform.Translate(-speed * Time.deltaTime, 0, 0);
+      }
+    }
+}
